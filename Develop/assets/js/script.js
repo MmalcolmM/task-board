68f96b67ee5334fe1adcefb1660ca36b30c3ace1
Malcolm Franklin <mac.mac5@yahoo.com>
2024-04-04 21:30:07 -0600

    updated Readme and JavaScript functions

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,38 +1,176 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+
+const todoList= $('#todoCards');
+const inProgressList = $('#inProgressCards');
+const doneList = $('#doneCards');
+const taskNameInput = $("#taskName");
+const dueDateInput =$("#dueDate");
+const taskDescriptionInput = $("#taskDescription");
+const deleteBtn = $('.deleteBtn');
+const lane = $('.laneList');
+lane.css('z-index', '-1');
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    
+    let nextId = JSON.parse(localStorage.getItem("nextId"));
+    if (!nextId) {nextId = 0} 
+    nextId++;
+    localStorage.setItem('nextId', nextId);
+    return nextId;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
 
+const newCard = $('<li>').css('z-index', '50').attr('data-taskId', task.id).addClass("taskCard");
+const newCardBody = $('<div>').addClass("card-body draggable").attr('data-nextId', task.id)
+const newTaskName = $('<h2>').addClass("card-title").text(task.title);
+const newDueDateField = $('<p>').addClass("card-text").text("Due date: " + task.dueDate);
+const newStatus = $('<p>').addClass("card-text cardStatusText").text("Status: " + task.status);
+const newDescription = $('<p>').addClass("card-text").text(task.description);
+const newDeleteButton = $('<button>').addClass("btn btn-danger custom-delete-btn deleteBtn").text('Delete').attr('data-nextId', task.id);
+
+
+
+if (task.dueDate && task.status !== 'done') {
+    const today = dayjs();
+    const dueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+    if (today.isSame(dueDate, 'day')) {
+      newCard.addClass('bg-warning text-white card draggable');
+    } else if (today.isAfter(dueDate)) {
+      newCard.addClass('bg-danger text-white card draggable');
+    } else {newCard.addClass('card bg-info draggable')}
+    
+} else {newCard.addClass('card bg-success draggable')}
+
+newCardBody.append(newTaskName, $('<hr>'), newDueDateField, newStatus, newDescription, newDeleteButton);
+newCard.append(newCardBody);
+
+
+return newCard;
 }
 
+
+
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
+function renderTaskList() {  
+
+    todoList.empty();
+    inProgressList.empty();
+    doneList.empty();
+    
+    $(function() {
+        $(".taskCard").draggable({
+            cancel: ".deleteBtn", // clicking an icon won't initiate dragging
+            revert: "invalid", // when not dropped, the item will revert back to its initial position 
+        });
+    });
+    
+    let tasks = JSON.parse(localStorage.getItem('tasks'))
+    if (!tasks) {tasks = []}
+    for (let task of tasks) {
+        if (task.status == "Time to start"){
+            todoList.append(createTaskCard(task));
+        } else if (task.status == "inProgress"){
+            inProgressList.append(createTaskCard(task));
+        } else if (task.status == "done"){
+            
+            doneList.append(createTaskCard(task));
+        }
+        
+    }
+
+
 
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    event.preventDefault();
 
+if (taskNameInput.val()=== '' || dueDateInput.val()=== ''|| taskDescriptionInput.val()=== '' ){
+    alert('You can\'t leave nothing blank \'round\ here');
+    return;
+} else {
+
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+const taskName = taskNameInput.val();
+const taskDueDate = dueDateInput.val();
+const taskDescription = taskDescriptionInput.val();
+console.log(taskList);
+const newTask = {
+    Name: taskName,
+    dueDate: taskDueDate,
+    description: taskDescription,
+    id: generateTaskId(),
+    status: "Time to start",
+}
+if (taskList == null) {
+    taskList = []
+}
+taskList.push(newTask);
+taskList = JSON.stringify(taskList);    
+localStorage.setItem('tasks', taskList);
+
+taskNameInput.val('')
+dueDateInput.val('')
+taskDescriptionInput.val('')
+console.log(newTask)
+
+renderTaskList();
+}
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
+function handleDeleteTask(event) {
+    const taskId = $(this).attr('data-task-id');
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    
+    tasks = tasks.filter(task => task.id !== taskId);
+    
+    tasks = JSON.stringify(tasks);
+    localStorage.setItem('tasks', tasks);
+    
+    location.reload();
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+    
+     const tasks = JSON.parse(localStorage.getItem('tasks'));    
+     let taskId = ui.draggable[0].dataset.taskid;
+     let dropStatus = $(this).attr('data-status')
+    
+     console.log(dropStatus)
+     console.log(taskId)
+for (let task of tasks){
+    if (task.id == taskId) {
+        task.status = dropStatus;
+    }    
+}
+localStorage.setItem('tasks', JSON.stringify(tasks));
+renderTaskList();
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    renderTaskList();
+    $(function() {
+        $( "#dueDate" ).datepicker();
+    });
+    
+    $(".taskForm").on('submit',handleAddTask);
+    $('.lane').droppable({
+        classes: {
+        "ui-droppable-active": "ui-state-highlight"
+        },
+        drop: handleDrop,
+    });
+    
 });
+
+$('.laneList').on('click', '.deleteBtn', handleDeleteTask)
+
+
--- README.md
+++ README.md
@@ -1,101 +1,28 @@
-# 05 Third-Party APIs: Task Board
+# Task Bord
 
-## Your Task
+## Description
 
-Create a simple task board application that allows a team to manage project tasks by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+A simple task board. 
+When it works you should be able to add your task, move it from lane to lane and delete it when done. 
 
-You'll need to use the [Day.js](https://day.js.org/en/) library to work with dates. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
+## Installation
 
-## User Story
+Install as you would normally clone a repo. For further instructions visit https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository
 
-```md
-AS A project team member with multiple tasks to organize
-I WANT a task board 
-SO THAT I can add individual project tasks, manage their state of progress and track overall project progress accordingly
-```
+## Usage
 
-## Acceptance Criteria
 
-```md
-GIVEN a task board to manage a project
-WHEN I open the task board
-THEN the list of project tasks is displayed in columns representing the task progress state (Not Yet Started, In Progress, Completed)
-WHEN I view the task board for the project
-THEN each task is color coded to indicate whether it is nearing the deadline (yellow) or is overdue (red)
-WHEN I click on the button to define a new task
-THEN I can enter the title, description and deadline date for the new task into a modal dialog
-WHEN I click the save button for that task
-THEN the properties for that task are saved in localStorage
-WHEN I drag a task to a different progress column
-THEN the task's progress state is updated accordingly and will stay in the new column after refreshing
-WHEN I click the delete button for a task
-THEN the task is removed from the task board and will not be added back after refreshing
-WHEN I refresh the page
-THEN the saved tasks persist
-```
+   ```md
+    ![Screen shot of the webpage showing different tasks added ](./develop/assets/images/Screenshot%202024-04-04%20at%206.25.48 PM.png)
+    ```
 
-The following animation demonstrates the application functionality:
+## Credits
 
-![A user adds three tasks to the task board and changes the state of two of them to in progress and then completion. The user then deletes the two cards in the done column.](./Assets/05-third-party-apis-homework-demo.gif)
+Dan - helped a lot with parts of the JavaScript. Thank you Dan. Follow his work here: https://github.com/djinjones.
 
-## Grading Requirements
+## License
 
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
+N/A
 
-This Challenge is graded based on the following criteria:
 
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-  * Uses the Day.js library to work with dates
-
-### Deployment: 32%
-
-* Application deployed at live URL
-
-* Application loads with no errors
-
-* Application GitHub URL submitted
-
-* GitHub repo contains application code
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate
-
-* Application user interface style is clean and polished
-
-* Application resembles the mock-up functionality provided in the Challenge instructions
-
-### Repository Quality: 13%
-
-* Repository has a unique name
-
-* Repository follows best practices for file structure and naming conventions
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages
-
-* Repository contains quality README file with description, screenshot, and link to deployed application
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application
-
-* The URL of the GitHub repository, with a unique name and a README describing the project
-
-- - -
-© 2024 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
-# task-board
+## Features
